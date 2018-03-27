4f211ac1da2e04c47e44bd891a1b711945c7b54e
Rich Boakes <rjb@port.ac.uk>
2018-03-27 06:43:13 +0100

    Add error handling on server to return 413 on oversize files.

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const md5File = require('md5-file/promise');
-const app = express();
 const multer = require('multer');
+const md5File = require('md5-file/promise');
+
 const config = require('./config.json');
-const upload = multer(config);
 
-app.use('/', express.static('webpages'));
-app.use(express.json());
+const app = express();
+const configuredMulter = multer(config);
+const single = configuredMulter.single('md5me');
 
-app.post('/upload', upload.single('md5me'), async (req, res) => {
-    res.json({
-        name: req.file.originalname,
-        hash: await md5File(req.file.path)
+function upload(req, res) {
+    single(req, res, async (error) => {
+        if (error) {
+            if (error.code === 'LIMIT_FILE_SIZE') {
+                res.status(413).send('Request Entity Too Large');
+            } else {
+                res.status(500).send('Server Error');
+            }
+            return;
+        }
+        res.json({
+            name: req.file.originalname,
+            hash: await md5File(req.file.path)
+        });
     });
-});
+}
+
+app.use(express.json());
+app.use('/', express.static('webpages'));
+app.post('/upload', upload);
 
 app.listen(8080, (e) => {
     console.log(`server ${e ? 'failed to start' : 'listening'}`);

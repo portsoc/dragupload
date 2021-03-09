d1d28dee3d6875c3a253c3f87aee5473bd271418
Jacek Kopecky <jacek@jacek.cz>
2021-03-09 13:17:14 +0000

    move error handling

--- server.js
+++ server.js
@@ -8,26 +8,25 @@ const app = express();
 const configuredMulter = multer(config);
 const single = configuredMulter.single('md5me');
 
-function upload(req, res) {
-    single(req, res, async (error) => {
-        if (error) {
-            if (error.code === 'LIMIT_FILE_SIZE') {
-                res.status(413).send('Request Entity Too Large');
-            } else {
-                res.status(500).send('Server Error');
-            }
-            return;
-        }
-        res.json({
-            name: req.file.originalname,
-            hash: await md5File(req.file.path)
-        });
+async function upload(req, res) {
+    res.json({
+        name: req.file.originalname,
+        hash: await md5File(req.file.path)
     });
 }
 
 app.use(express.json());
 app.use('/', express.static('webpages'));
-app.post('/upload', upload);
+app.post('/upload', single, upload);
+
+// error handling
+app.use((error, req, res, next) => {
+    if (error.code === 'LIMIT_FILE_SIZE') {
+        res.status(413).send('Request Entity Too Large');
+    } else {
+        res.status(500).send('Server Error');
+    }
+});
 
 app.listen(8080, (e) => {
     console.log(`server ${e ? 'failed to start' : 'listening'}`);

4804d449163c6c5b2df7e807d4469ac46c9b91c6
Jacek Kopecky <jacek@jacek.cz>
2021-03-09 14:12:21 +0000

    :art:

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const configuredMulter = multer(config);
 const single = configuredMulter.single('md5me');
 
 async function upload(req, res) {
-    res.json({
+    const response = {
         name: req.file.originalname,
         hash: await md5File(req.file.path)
-    });
+    };
+    res.json(response);
 }
 
-app.use(express.json());
 app.use('/', express.static('webpages'));
 app.post('/upload', single, upload);
 
